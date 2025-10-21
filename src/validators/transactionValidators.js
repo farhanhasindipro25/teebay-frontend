e5b4238c6a5036c1f7b00ff016632dd78e886fcb
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2025-10-22 03:43:35 +0600

    fix: Fixed rental timeline overlap

--- src/validators/transactionValidators.js
+++ src/validators/transactionValidators.js
@@ -1,14 +1,33 @@
 import * as yup from "yup";
+import dayjs from "dayjs";
 
 export const rentProductSchema = yup.object().shape({
     rentStartDate: yup
         .date()
         .required("Start date is required")
-        .min(new Date(), "Start date cannot be in the past")
-        .typeError("Please select a valid start date"),
+        .typeError("Please select a valid start date")
+        .test("not-in-past", "Start date cannot be in the past", (value) => {
+            if (!value) return false;
+            const today = dayjs().startOf("day");
+            const start = dayjs(value).startOf("day");
+            return start.isAfter(today) || start.isSame(today);
+        }),
+
     rentEndDate: yup
         .date()
         .required("End date is required")
-        .min(yup.ref("rentStartDate"), "End date must be after start date")
-        .typeError("Please select a valid end date"),
+        .typeError("Please select a valid end date")
+        .test(
+            "is-after-start",
+            "End date must be after start date",
+            function (value) {
+                const { rentStartDate } = this.parent;
+                if (!value || !rentStartDate) return false;
+
+                const start = dayjs(rentStartDate).startOf("day");
+                const end = dayjs(value).startOf("day");
+
+                return end.isAfter(start);
+            }
+        ),
 });
